9d95ce3964e48a3cac5babb2002fd41d4cb715a8
victorperezpiqueras <victorperezpiqueras@gmail.com>
2023-01-26 20:08:26 +0100

    rename jira token

--- monte_carlo.py
+++ monte_carlo.py
@@ -11,11 +11,11 @@ from dotenv import load_dotenv
 
 class MonteCarloSimulation:
 
-    def __init__(self, jira_url, jira_username, jira_password, project_code, show_plots=False):
+    def __init__(self, jira_url, jira_username, jira_token, project_code, show_plots=False):
 
         self.jira_url = jira_url
         self.jira_username = jira_username
-        self.jira_password = jira_password
+        self.jira_token = jira_token
         self.project_code = project_code
         self.show_plots = show_plots
 
@@ -23,11 +23,12 @@ class MonteCarloSimulation:
         issues = []
         moreResults = True
         while moreResults:
-            response = requests.get(f"{self.jira_url}/search", auth=(self.jira_username, self.jira_password),
+            response = requests.get(f"{self.jira_url}/search", auth=(self.jira_username, self.jira_token),
                                     params=params)
             # Check if the request was successful
             if response.status_code != 200:
-                raise Exception("Error: Unable to retrieve issues from Jira API")
+                raise Exception(
+                    "Error: Unable to retrieve issues from Jira API")
             # Parse the JSON data from the response
             data = json.loads(response.text)
             # Add the issues to the list
@@ -48,11 +49,13 @@ class MonteCarloSimulation:
         }
         for issue in issues:
             # Send the GET request to the Jira API and store the response in a variable
-            response = requests.get(f"{issue['self']}", auth=(self.jira_username, self.jira_password), params=params)
+            response = requests.get(f"{issue['self']}", auth=(
+                self.jira_username, self.jira_token), params=params)
 
             # Check if the request was successful
             if response.status_code != 200:
-                print(f"Error: Unable to retrieve history of changes for issue {issue}")
+                print(
+                    f"Error: Unable to retrieve history of changes for issue {issue}")
                 continue
 
             # Parse the JSON data from the response
@@ -85,7 +88,8 @@ class MonteCarloSimulation:
 
         # Get the current date and calculate the start and end date for last year
         now = datetime.datetime.now()
-        last_year_start = (now - datetime.timedelta(days=365)).strftime("%Y-%m-%d")
+        last_year_start = (now - datetime.timedelta(days=365)
+                           ).strftime("%Y-%m-%d")
         last_year_end = now.strftime("%Y-%m-%d")
 
         # check if file in output, load from it:
@@ -97,7 +101,7 @@ class MonteCarloSimulation:
         else:
             story_point_field = [x for x in
                                  json.loads(requests.get(f"{self.jira_url}/field",
-                                                         auth=(self.jira_username, self.jira_password)).text) if
+                                                         auth=(self.jira_username, self.jira_token)).text) if
                                  "Story Points" in x['name']][0]
 
             # Define the Jira API query parameters
@@ -152,7 +156,8 @@ class MonteCarloSimulation:
 
         cycle_times = [issue["fields"]["cycle_time"].total_seconds() for issue in issues if
                        issue["fields"]["cycle_time"]]
-        cycle_times_days = [cycle_times_day / 86400 for cycle_times_day in cycle_times]
+        cycle_times_days = [cycle_times_day /
+                            86400 for cycle_times_day in cycle_times]
         real_mean = np.mean(cycle_times_days)
         real_std = np.std(cycle_times_days)
 
@@ -160,19 +165,23 @@ class MonteCarloSimulation:
         dist = stats.norm(real_mean, real_std)
         x = np.linspace(-100, 100, 200)
         y = dist.pdf(x)
-        print(f"REAL DATA: Mean : {real_mean} and Standard deviation : {real_std}")
-        plt.plot(x, y, color='black', linestyle='solid', label='Probability Density Function of real data')
+        print(
+            f"REAL DATA: Mean : {real_mean} and Standard deviation : {real_std}")
+        plt.plot(x, y, color='black', linestyle='solid',
+                 label='Probability Density Function of real data')
 
         # Generate samples from the probability distribution generated using the cycle times
         samples = stats.norm.rvs(real_mean, real_std, size=sample_size)
         sampled_mean = np.mean(samples)
         sampled_std = np.std(samples)
-        print(f"SAMPLES: Mean : {sampled_mean} and Standard deviation : {sampled_std}")
+        print(
+            f"SAMPLES: Mean : {sampled_mean} and Standard deviation : {sampled_std}")
         # Calculate statistics for the samples
         t_stat, p_value = stats.ttest_ind(cycle_times_days, samples)
         print(f"t-statistic: {t_stat}")
         print(f"p-value: {p_value}")
-        plt.hist(samples, bins=60, density=True, rwidth=0.9, label=f'Estimated from samples with p-value = {p_value}')
+        plt.hist(samples, bins=60, density=True, rwidth=0.9,
+                 label=f'Estimated from samples with p-value = {p_value}')
 
         # Calculate time of completing an issue based on the sample distribution with X% confidence
         confidence = 85
@@ -196,21 +205,25 @@ class MonteCarloSimulation:
 
     def get_story_points_relationship(self, issues):
         plt.figure(figsize=(14, 8))
-        story_points = [issue["fields"]["story_points"] for issue in issues if issue["fields"]["story_points"]]
+        story_points = [issue["fields"]["story_points"]
+                        for issue in issues if issue["fields"]["story_points"]]
         cycle_times = [issue["fields"]["cycle_time"].total_seconds() for issue in issues if
                        issue["fields"]["cycle_time"]]
-        cycle_times_days = [cycle_times_day / 86400 for cycle_times_day in cycle_times]
+        cycle_times_days = [cycle_times_day /
+                            86400 for cycle_times_day in cycle_times]
         plt.scatter(story_points, cycle_times_days)
 
         # add a linear regression
-        slope, intercept, r_value, p_value, std_err = stats.linregress(story_points, cycle_times_days)
+        slope, intercept, r_value, p_value, std_err = stats.linregress(
+            story_points, cycle_times_days)
         line = slope * np.array(story_points) + intercept
         plt.plot(story_points, line, 'r-', label='Regression line')
 
         plt.xticks([1, 2, 3, 5, 8, 13, 20])
         plt.xlabel('Story Points')
         plt.ylabel('Cycle Time in Days')
-        plt.title("Cycle Time vs Story Points (R^2= {:.2f})".format(r_value ** 2))
+        plt.title(
+            "Cycle Time vs Story Points (R^2= {:.2f})".format(r_value ** 2))
 
         if self.show_plots:
             plt.show()
@@ -226,16 +239,18 @@ if __name__ == "__main__":
     # Jira API endpoint and credentials
     jira_url = os.environ.get("JIRA_URL")
     jira_username = os.environ.get("JIRA_USER")
-    jira_password = os.environ.get("JIRA_PASSWORD")
+    jira_token = os.environ.get("jira_token")
     project_code = os.environ.get("JIRA_PROJECT_CODE")
 
-    monte_carlo = MonteCarloSimulation(jira_url, jira_username, jira_password, project_code, show_plots=True)
+    monte_carlo = MonteCarloSimulation(
+        jira_url, jira_username, jira_token, project_code, show_plots=True)
 
     issues = monte_carlo.get_all_issues_data()
 
     monte_carlo.calculate_cycle_times(issues, filter_not_in_progress=False)
 
-    cycle_time_issues = [issue for issue in issues if issue["fields"]["cycle_time"]]
+    cycle_time_issues = [
+        issue for issue in issues if issue["fields"]["cycle_time"]]
 
     df = pd.DataFrame(cycle_time_issues)
     df.to_json("output/cycle_time_issues.json")
